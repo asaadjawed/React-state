8d59b4ea62ff6550c44b0459f22d1dfc9ba21738
asaadjawed <asaadjawed70@gmail.com>
2021-04-05 14:20:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './component/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+  constructor(){
+    super()
+
+
+    this.defaultEmail = "asaadjawed70@gmail.com";
+    this.defaultPass  = "123";
+
+    this.state ={
+      email : "",
+      pass : "",
+    }
+  }
+
+ 
+ handleSetEmail=(event)=>{
+
+  this.setState({
+
+    email:event.target.value,
+    pass:this.state.pass,
+   
+  })
+  
+ }
+
+
+
+
+
+ handleSetPassword=(event)=>{
+
+  this.setState({
+    pass: event.target.value,
+    email:this.state.email,
+
+  })
+ }
+
+
+ handleSubmit=(e)=>{
+
+  e.preventDefault();
+   if(this.state.email==this.defaultEmail && this.state.pass==this.defaultPass){
+     console.log("You'r Welcome");
+   }else{
+     console.log("You'r entered wrong Password, Plz try again");
+   }
+  
+ }
+
+  render(){
+     return (
+
+    <div>
+       
+     <form>
+
+    Email: <input value={this.state.email} onChange={this.handleSetEmail} type="email" name="email" placeholder="Enter an email" id="email"/> <br/>
+
+    Password: <input value={this.state.pass} onChange={this.handleSetPassword} type="password" name="password" placeholder="Enter a password" id="password"/>
+    
+      <button  onClick={this.handleSubmit} type="submit">submit</button>
+     </form>
+          
+      </div>
+    );
+  }
 }
 
-export default App;
---
+++ src/component/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+                <h1>Hello World</h1>
+                <h1>Hello World</h1>
+                <h1>Hello World</h1>
+                <h1>Hello World</h1>
+                <h1>Hello World</h1> <h1>Hello World</h1>
+
+
+            </div>
+        )
+    }
+}
+
+export default Header
